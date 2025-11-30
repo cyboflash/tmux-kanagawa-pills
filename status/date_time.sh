514317a393ce8dde9c7532bf8cd42a41720022b9
rail shafigulin <rashafig@amazon.com>
2025-11-29 22:24:50 -0800

    Remove whitespace

--- status/date_time.sh
+++ status/date_time.sh
@@ -2,6 +2,6 @@ show_date_time() {
   local color=$(get_tmux_option "@kanagawa_date_time_color" "$THM_GRAY")
   local icon=$(get_tmux_option "@kanagawa_date_time_icon" "󰃰")
   local format=$(get_tmux_option "@kanagawa_date_time_text" "%Y-%m-%d %H:%M")
-  
+
   build_module "$format" "$icon" "$color"
 }
--- utils/module_utils.sh
+++ utils/module_utils.sh
@@ -10,10 +10,17 @@ build_module() {
   local left_sep=$(get_tmux_option "@kanagawa_left_sep" "")
   local right_sep=$(get_tmux_option "@kanagawa_right_sep" "")
   local bg_bar=$(get_tmux_option "@kanagawa_bar_bg" "default")
-  
+
   # Fetch surface color (User Override > Theme Default)
   # We rely on the theme being sourced in main to provide $THM_BG_SURFACE default
   local bg_surface=$(get_tmux_option "@kanagawa_bg_surface" "$THM_BG_SURFACE")
+
+  local connect=$(get_tmux_option "@kanagawa_status_connect_separator" "no")
+  local spacer=" "
+
+  if [ "$connect" == "yes" ]; then
+    spacer=""
+  fi
 
   echo "#[fg=$module_color,bg=$bg_bar]$left_sep#[fg=$bg_surface,bg=$module_color,bold]$module_icon $module_text#[fg=$module_color,bg=$bg_bar]$right_sep "
 }
